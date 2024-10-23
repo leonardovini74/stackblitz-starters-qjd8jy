9bb2bfc7a8699c6989b05e5d30d5a0bb21c77eaf
leonardovini74 <git@stackblitz.com>
2024-10-23 19:32:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-qjd8jy
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/leonardovini74/stackblitz-starters-qjd8jy)
---
+++ script.js
@@ -0,0 +1,17 @@
+async function convert() {
+  const amount = document.getElementById('amount').value;
+  const fromCurrency = document.getElementById('fromCurrency').value;
+  const toCurrency = document.getElementById('toCurrency').value;
+
+  if (amount === '' || amount <= 0) {
+      alert('Por favor, insira um valor válido.');
+      return;
+  }
+
+  const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
+  const data = await response.json();
+  const rate = data.rates[toCurrency];
+  const result = amount * rate;
+
+  document.getElementById('result').innerText = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
+}
